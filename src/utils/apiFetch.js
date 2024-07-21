93b94fa0d63007c60953084d0096077a40122e24
Naveen N <nnaveen112233@gmail.com>
2024-07-22 00:56:54 +0530

    Backend url

--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,5 +1,5 @@
 export const apiFetch = async (url, options) => {
-  // url = "https://social-media-backend-8qt2.onrender.com" + url;
+  url = "https://profinsta-backend.netlify.app" + url;
   // url = "http://localhost:3001" + url;
   return await fetch(url, options);
 };
